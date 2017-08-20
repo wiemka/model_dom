c79d79bebfeec6c707e429835dea8a26da615b3a
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-20 14:57:13 +0200

    ćwiczenie events part 2

--- js/events.js
+++ js/events.js
@@ -5,6 +5,16 @@ function klikHeader() {
     //alert("Klink"); //dodaje alert w sensie wyskakującego okienka
 }
 
+function alarm(event) {
+    event.preventDefault(); //zapobiegamy domyślnej akcji
+    console.log("Kliknięto w link");
+    console.log(event.type);
+}
+
+var secondLink = document.getElementsByTagName("a")[1];
+
+secondLink.onclick = alarm;
+
 document.getElementById("title_header").onclick = klikHeader; //wywołaj funkcję na elemencie o id "title_header"
 
 var paragraf = document.getElementsByTagName("p")[0];
@@ -12,3 +22,5 @@ var paragraf = document.getElementsByTagName("p")[0];
 paragraf.addEventListener("click", klikHeader);
 
 //document.getElementsByTagName("p")[0].addEventListener("click", klikHeader); //skrócona wersja
+
+
